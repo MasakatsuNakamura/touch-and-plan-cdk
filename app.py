eda172a62efbac04bd8f16a0d82e7de2565db768
中村昌克 <nakamura.masakatsu@gmail.com>
2021-08-17 09:10:37 +0900

    テスト用リソースの追加

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from touch_and_plan_cdk.touch_and_plan_cdk_stack import TouchAndPlanCdkStack
 
 
 app = core.App()
-TouchAndPlanCdkStack(app, "TouchAndPlanCdkStack",
+TouchAndPlanCdkStack(app, "TouchAndPlanCdkStack", env=core.Environment(region="ap-northeast-1")
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
--- touch_and_plan_cdk/touch_and_plan_cdk_stack.py
+++ touch_and_plan_cdk/touch_and_plan_cdk_stack.py
@@ -6,10 +6,67 @@ from aws_cdk import core as cdk
 # being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import core
 
+from aws_cdk import (
+  aws_s3 as s3,
+  aws_ec2 as ec2,
+  core as cdk
+)
 
 class TouchAndPlanCdkStack(cdk.Stack):
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+  def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+    # The code that defines your stack goes here
+    bucket = s3.Bucket(self,
+      "MyFirstBucket",
+      versioned=True,)
+
+    cidr = '10.0.0.0/21'
+    vpc = ec2.Vpc(
+      self,
+      id='test-vpc',
+      cidr=cidr,
+      nat_gateways=1,
+      subnet_configuration=[
+        ec2.SubnetConfiguration(
+          cidr_mask=24,
+          name='public',
+          subnet_type=ec2.SubnetType.PUBLIC,
+        ),
+        ec2.SubnetConfiguration(
+          cidr_mask=24,
+          name='private',
+          subnet_type=ec2.SubnetType.PRIVATE,
+        ),
+      ],
+    )
+
+    security_group = ec2.SecurityGroup(
+      self,
+      id='test-security-group',
+      vpc=vpc,
+      security_group_name='test-security-group'
+    )
+
+    security_group.add_ingress_rule(
+      peer=ec2.Peer.ipv4(cidr),
+      connection=ec2.Port.tcp(22),
+    )
+
+    image_id = ec2.AmazonLinuxImage(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2).get_image(self).image_id
+
+    ec2.CfnInstance(
+      self,
+      id='testec2',
+      availability_zone="ap-northeast-1a",
+      image_id=image_id,
+      instance_type="t3.micro",
+      key_name='testkey',
+      security_group_ids=[security_group.security_group_id],
+      subnet_id=vpc.private_subnets[0].subnet_id,
+      tags=[{
+        "key": "Name",
+        "value": "testec2"
+      }]
+    )
